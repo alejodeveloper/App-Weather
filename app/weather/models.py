e92b09e66c0e09b2d5556be8b42e2684940a8a75
alejodev <alejo.pm92@gmail.com>
2020-04-04 11:38:31 -0500

    [add] models

--- app/weather/models.py
+++ app/weather/models.py
@@ -1,3 +1,54 @@
+"""
+weather.models
+--------------
+Model for the weather app
+"""
+
 from django.db import models
 
-# Create your models here.
+
+class CityWeather(models.Model):
+    """
+    Class model for city
+    """
+    name = models.CharField(max_length=255, null=False, blank=False)
+    slug = models.CharField(max_length=25, null=False, blank=False)
+    country_slug = models.CharField(max_length=25, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["name", "slug"]),
+            models.Index(fields=["country_slug"])
+        ]
+
+
+class LogResponse(models.Model):
+    """
+    Class model for logging the API response
+    """
+    log_date = models.DateField(blank=False, null=False)
+    response = models.TextField(null=False, blank=False)
+    status_response = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["log_date", "status_response"])
+        ]
+
+
+class CityWeatherLog(models.Model):
+    """
+    Model to associate the response to the model
+    """
+    city = models.ForeignKey(
+        CityWeather, null=False, blank=False, on_delete=models.SET_NULL
+    )
+    log = models.ForeignKey(
+        LogResponse, null=False, blank=False, on_delete=models.SET_NULL
+    )
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
